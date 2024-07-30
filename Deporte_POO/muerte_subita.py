bff801e754a2c180cb6db328944e2ffc81cb866a
Nikooll Camacho <166524335+Nikooll@users.noreply.github.com>
2024-07-30 10:37:27 -0500

    Add files via upload

---
+++ Deporte_POO/muerte_subita.py
@@ -0,0 +1,11 @@
+from sistema_campeonato import SistemaCampeonato
+
+class MuerteSubita(SistemaCampeonato):
+    def __init__(self, rondas):
+        self.rondas = rondas
+
+    def jugar_partido(self):
+        pass
+
+    def calcular_resultados(self):
+        pass
---
+++ Deporte_POO/suspension.py
@@ -0,0 +1,3 @@
+class Suspension:
+    def __init__(self, tiempo_suspencion):
+        self.tiempo_suspencion = tiempo_suspencion
